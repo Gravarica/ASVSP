40bdbafcaacbde23191c592c48895ce30515b96e
Gravarica <93195018+Gravarica@users.noreply.github.com>
2024-01-03 20:24:58 +0100

    Added streaming take one

--- implementacija/stream/producer/producer.py
+++ implementacija/stream/producer/producer.py
@@ -1,20 +1,43 @@
 import json
 import websocket
+import os, time
 import pandas as pd
+from kafka import KafkaProducer 
+import kafka.errors
 
-assets = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT']
+KAFKA_BROKER = os.environ["KAFKA_BROKER"]
+TOPIC = 'btc-usdt'
+key = 0
 
-assets = [coin.lower() + '@kline_1m' for coin in assets]
+while True:
+    try:
+        producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER.split(","))
+        print("Connected to Kafka!")
+        break
+    except kafka.errors.NoBrokersAvailable as e:
+        print(e)
+        time.sleep(3)
 
-assets = '/'.join(assets)
+def setup_socket():
+    assets = ['BTCUSDT']
+
+    assets = [coin.lower() + '@kline_1m' for coin in assets]
+
+    assets = '/'.join(assets)
+
+    socket = "wss://stream.binance.com:9443/stream?streams="+assets
+
+    return socket
 
 def on_message(ws, message):
     message = json.loads(message)
-    transform(message)
+    key, data = transform(message)
+    producer.send(TOPIC, key=bytes(key, 'utf-8'), value=bytes(data, 'utf-8'))
 
 def transform(source):
     kdata = source['data']['k']
-    extracted_data = {
+    key = key + 1
+    extracted_data = { 
         'pair' : source['data']['s'],
         'open_time' : pd.to_datetime(kdata['t'], unit='ms'),
         'open' : kdata['o'],
@@ -28,12 +51,8 @@ def transform(source):
         'taker_buy_quote_asset_volume' : kdata['Q']
     }
     
-    df = pd.DataFrame([extracted_data])
-    print(df.head().to_string())
+    return key, extracted_data
 
+ws = websocket.WebSocketApp(setup_socket(), on_message=on_message)
 
-
-socket = "wss://stream.binance.com:9443/stream?streams="+assets
-
-ws = websocket.WebSocketApp(socket, on_message=on_message)
 ws.run_forever()
